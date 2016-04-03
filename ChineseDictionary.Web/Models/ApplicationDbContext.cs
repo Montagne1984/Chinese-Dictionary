2fcc7da06199a1cdd09fb88a2ee9629f0cf6277e
Montagne1984 <shifeng222@gmail.com>
2016-04-03 19:19:47 +0800

    add Area

--- ChineseDictionary.Web/Models/ApplicationDbContext.cs
+++ ChineseDictionary.Web/Models/ApplicationDbContext.cs
@@ -12,6 +12,7 @@ namespace ChineseDictionary.Web.Models
         public DbSet<Consonant> Consonants { get; set; }
         public DbSet<Vowel> Vowels { get; set; }
         public DbSet<ToneType> ToneTypes { get; set; }
+        public DbSet<Area> Areas { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
---
+++ ChineseDictionary.Web/Models/Area.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChineseDictionary.Web.Models
+{
+    public class Area
+    {
+        public int AreaId { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; }
+    }
+}
