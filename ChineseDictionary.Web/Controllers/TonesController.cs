efd3ef0713cdee1f67b4655382a99393b1ac68f9
Montagne1984 <shifeng222@gmail.com>
2016-04-04 16:41:14 +0800

    fix tones pages

--- ChineseDictionary.Web/Controllers/TonesController.cs
+++ ChineseDictionary.Web/Controllers/TonesController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNet.Mvc;
 using Microsoft.AspNet.Mvc.Rendering;
 using Microsoft.Data.Entity;
 using ChineseDictionary.Web.Models;
+using ChineseDictionary.Web.ViewModels.Tones;
 
 namespace ChineseDictionary.Web.Controllers
 {
@@ -19,7 +20,7 @@ namespace ChineseDictionary.Web.Controllers
         // GET: Tones
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Tones.ToListAsync());
+            return View(await _context.Tones.Include(m => m.Area).Include(m => m.ToneType).ToListAsync());
         }
 
         // GET: Tones/Details/5
@@ -42,23 +43,33 @@ namespace ChineseDictionary.Web.Controllers
         // GET: Tones/Create
         public IActionResult Create()
         {
-            ViewBag.Areas = new SelectList(_context.Areas, "Id", "Name");
-            ViewBag.ToneTypes = new SelectList(_context.ToneTypes, "Id", "Name");
+            ViewBag.Areas = _context.Areas;
+            ViewBag.ToneTypes = _context.ToneTypes;
             return View();
         }
 
         // POST: Tones/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Tone tone)
+        public async Task<IActionResult> Create(CreateToneViewModel model)
         {
             if (ModelState.IsValid)
             {
-                _context.Tones.Add(tone);
+                var area = await _context.Areas.SingleAsync(m => m.Id == model.AreaId);
+                if (area == null)
+                {
+                    return HttpNotFound();
+                }
+                var toneType = await _context.ToneTypes.SingleAsync(m => m.Id == model.ToneTypeId);
+                if (toneType == null)
+                {
+                    return HttpNotFound();
+                }
+                _context.Tones.Add(new Tone { Area = area, ToneType = toneType, Value = model.Value });
                 await _context.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
-            return View(tone);
+            return View(model);
         }
 
         // GET: Tones/Edit/5
@@ -69,26 +80,46 @@ namespace ChineseDictionary.Web.Controllers
                 return HttpNotFound();
             }
 
-            Tone tone = await _context.Tones.SingleAsync(m => m.Id == id);
+            var tone = await _context.Tones.Include(m => m.Area).Include(m => m.ToneType).SingleAsync(m => m.Id == id);
             if (tone == null)
             {
                 return HttpNotFound();
             }
-            return View(tone);
+            ViewBag.Areas = _context.Areas;
+            ViewBag.ToneTypes = _context.ToneTypes;
+            return View(new EditToneViewModel { Id = tone.Id, AreaId = tone.Area.Id, ToneTypeId = tone.ToneType.Id, Value = tone.Value });
         }
 
         // POST: Tones/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Tone tone)
+        public async Task<IActionResult> Edit(EditToneViewModel model)
         {
             if (ModelState.IsValid)
             {
+                var tone = await _context.Tones.SingleAsync(m => m.Id == model.Id);
+                if (tone == null)
+                {
+                    return HttpNotFound();
+                }
+                var area = await _context.Areas.SingleAsync(m => m.Id == model.AreaId);
+                if (area == null)
+                {
+                    return HttpNotFound();
+                }
+                var toneType = await _context.ToneTypes.SingleAsync(m => m.Id == model.ToneTypeId);
+                if (toneType == null)
+                {
+                    return HttpNotFound();
+                }
+                tone.Area = area;
+                tone.ToneType = toneType;
+                tone.Value = model.Value;
                 _context.Update(tone);
                 await _context.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
-            return View(tone);
+            return View(model);
         }
 
         // GET: Tones/Delete/5
@@ -100,7 +131,7 @@ namespace ChineseDictionary.Web.Controllers
                 return HttpNotFound();
             }
 
-            Tone tone = await _context.Tones.SingleAsync(m => m.Id == id);
+            Tone tone = await _context.Tones.Include(m => m.Area).Include(m => m.ToneType).SingleAsync(m => m.Id == id);
             if (tone == null)
             {
                 return HttpNotFound();
